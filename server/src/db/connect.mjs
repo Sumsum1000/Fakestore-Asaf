332308b437ff64514168c85e36de7c00db7a1f80
Asaf Levi <asafl14@yahoo.com>
2021-09-12 19:42:01 +0300

    url fixed

--- server/src/db/connect.mjs
+++ server/src/db/connect.mjs
@@ -2,10 +2,13 @@ import mongoose from 'mongoose';
 
 //mongoose.connect('mongodb://localhost:27017/fakestore', {useNewUrlParser: true, useUnifiedTopology: true});
 
-const dbURL = process.env.DB_URL || 'mongodb.//localhost:27017/fakestore'
+const dbURL = process.env.DB_URL || 'mongodb://localhost:27017/fakestore'
 
-
-//'mongodb+srv://fakestoreuser:fakestoreuser@cluster0.aqk1t.mongodb.net/fakestoreuser'
+//mongodb+srv://fakestoreuser:fakestoreuser@cluster0.aqk1t.mongodb.net/fakestore
+//'mongodb+srv://fakestoreuser:fakestoreuser@cluster0.aqk1t.mongodb.net/fakestore'
 mongoose.connect(dbURL, {useNewUrlParser: true, useUnifiedTopology: true});
 
+
+
+//mongodb+srv://fakestoreuser:<password>@cluster0.aqk1t.mongodb.net/fakestore
 
