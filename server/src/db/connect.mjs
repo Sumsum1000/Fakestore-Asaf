98b0728b76cfde14a1d39dda960e185375f0fc12
Asaf Levi <asafl14@yahoo.com>
2021-09-12 19:28:33 +0300

    commit

--- server/src/db/connect.mjs
+++ server/src/db/connect.mjs
@@ -2,7 +2,10 @@ import mongoose from 'mongoose';
 
 //mongoose.connect('mongodb://localhost:27017/fakestore', {useNewUrlParser: true, useUnifiedTopology: true});
 
+const dbURL = process.env.DB_URL || 'mongodb.//localhost:27017/fakestore'
 
-mongoose.connect('mongodb+srv://fakestoreuser:fakestoreuser@cluster0.aqk1t.mongodb.net/fakestoreuser', {useNewUrlParser: true, useUnifiedTopology: true});
+
+//'mongodb+srv://fakestoreuser:fakestoreuser@cluster0.aqk1t.mongodb.net/fakestoreuser'
+mongoose.connect(dbURL, {useNewUrlParser: true, useUnifiedTopology: true});
 
 
