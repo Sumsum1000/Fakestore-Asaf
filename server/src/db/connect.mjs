734cb2adb39090f7a6f0e8070b15c5f55e2ffc79
Asaf Levi <asafl14@yahoo.com>
2021-09-12 16:23:04 +0300

    connect to atlas

--- server/src/db/connect.mjs
+++ server/src/db/connect.mjs
@@ -1,3 +1,8 @@
 import mongoose from 'mongoose';
 
-mongoose.connect('mongodb://localhost:27017/fakestore', {useNewUrlParser: true, useUnifiedTopology: true});
+//mongoose.connect('mongodb://localhost:27017/fakestore', {useNewUrlParser: true, useUnifiedTopology: true});
+
+
+mongoose.connect('mongodb+srv://fakestoreuser:fakestoreuser@cluster0.aqk1t.mongodb.net/fakestoreuser', {useNewUrlParser: true, useUnifiedTopology: true});
+
+
